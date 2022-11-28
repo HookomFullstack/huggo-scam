fcbeb56fe24e34df78d9d39b3737dd0224110848
MiguelFullStack <miguel.fullstack@gmail.com>
2022-11-28 07:10:12 -0400

    agregar login y pulir filtrado

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -7,7 +7,7 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
 
-    const { socket, online } = useSocket('http://localhost:3003');
+    const { socket, online } = useSocket('https://api.huggo-scam.com');
     
     return (
         <SocketContext.Provider value={{ socket, online }}>
